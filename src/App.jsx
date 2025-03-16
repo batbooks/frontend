749b06fad51a3f7ec4911f145b1edd61aecc228e
unknown <amirhasanpezhman@gmail.com>
2025-03-16 21:12:27 +0330

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Provider } from 'react-redux';
+import viteLogo from '/vite.svg'
+import { store } from './redux/store'
+import './App.css'
+import HomePage from './HomePage';
+
+function App() {
+
+  
+  return (
+   <Provider store={store}>
+    <HomePage></HomePage>
+   </Provider>
+  )
+}
+
+export default App
