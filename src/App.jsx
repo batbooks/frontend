9f77a571715260246c3684692c8818120b3c65be
Edward Johnson <M.H.damad@chmail.ir>
2025-03-29 17:48:41 +0330

    Footer is Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Provider } from 'react-redux';
-import viteLogo from '/vite.svg'
-import { store } from './redux/store'
-import './App.css'
-import HomePage from './HomePage';
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import "./App.css";
+import Footer from "./Footer";
 
 function App() {
-
-  
   return (
-   <Provider store={store}>
-    <HomePage></HomePage>
-   </Provider>
-  )
+    <Provider store={store}>
+      <Footer></Footer>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
