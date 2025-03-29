6f235c1c25126e561c2ae5ddfac2e2f11bf966d2
Edward Johnson <M.H.damad@chmail.ir>
2025-03-30 02:26:25 +0330

    Foldering Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import "./App.css";
-import Another_User_Profile from "./Another_User_Profile";
+import AppRoutes from "./routes";
 
 function App() {
   return (
     <Provider store={store}>
-      <Another_User_Profile></Another_User_Profile>
+      <AppRoutes />
     </Provider>
   );
 }
---
+++ src/routes.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router";
+import Signup from "./features/Auth/Sign up/signup";
+import Login from "./features/Auth/Login/login";
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/auth/signup" element={<Signup />}></Route>
+        <Route path="/auth/login" element={<Login />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRoutes;
