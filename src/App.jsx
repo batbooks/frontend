4b283dedb26d3cf6c3563d038a15f69b0b2c3051
unknown <amirhasanpezhman@gmail.com>
2025-04-01 19:21:23 +0330

    noth

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Provider } from 'react-redux';
-import viteLogo from '/vite.svg'
-import { store } from './redux/store'
+
+
+
+
 import './App.css'
 
-import Bookpage from './features/Bookpage/Bookpage';
+
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import {} from 'react-redux';
 import { decrement, increment, incrementByAmount } from './redux/infoSlice';
 
 
@@ -17,7 +16,7 @@ import { decrement, increment, incrementByAmount } from './redux/infoSlice';
 
 
 
-import { Provider } from "react-redux";
+import { useSelector, useDispatch , Provider } from "react-redux";
 import { store } from "./redux/store";
 import AppRoutes from "./routes";
 
