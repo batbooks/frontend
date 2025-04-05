36c5dd88d832ac79a9ac35ed3cce19b5bcd1db92
Alisalimi11 <www.asalimi1399@gmail.com>
2025-04-05 16:12:56 +0330

    login/sign up/otp ui is completed

--- src/features/Auth/SignUp/signup.jsx
+++ src/features/Auth/SignUp/signup.jsx
@@ -21,7 +21,6 @@ function Signup() {
   }
 
   return (
-<<<<<<< HEAD:src/features/Auth/Sign up/signup.jsx
     <div className="w-[100vw] h-[100vh] bg-[#D9F0FF]">
       <div className="flex gap-1 items-center ">
         <h2 className="text-[24px] mt-1.5 ml-2 font-bold text-[#002D54]">
@@ -109,48 +108,6 @@ function Signup() {
             )}
           </div>
           <button className="bg-[#2663CD] shadow-lg shadow-[#000]/25 text-white mx-auto flex justify-center items-center  w-[143px] h-[38px] rounded-[46px] cursor-pointer focus:shadow-none focus:bg-[#2663CD]/90 focus:outline-none focus:ring-[#2663CD] focus:ring-offset-2 focus:ring-[2px] hover:bg-[#2663CD]/90 active:bg-[#2663CD]/30 active:duration-300 active:outline-none active:ring-0 active:ring-offset-0 disabled:cursor-auto disabled:shadow-none disabled:bg-[#2663CD]/60 disabled:ring-0 disabled:ring-offset-0 ">
-=======
-    <div className="h-[100vh] w-[100vw] bg-[#D9F0FF]">
-      <div className="flex items-center gap-1">
-        <img
-          src="/photos/logo.png"
-          alt="mid-right"
-          className="w-[6vw] pt-1.5"
-        />
-      </div>
-
-      <main className="m-auto h-[55vh] w-[52vw] justify-center rounded-[13px] bg-[#A4C0ED]">
-        <h2 className="mt-auto text-center text-[20px] font-bold">
-          به batbooks خوش آمدید
-        </h2>
-        <h3 className="mb-14 text-center text-[16px]">
-          برای ادامه دادن ثبت نام کنید
-        </h3>
-        <form typeof="submit" className="flex-col" onSubmit={handleSubmit}>
-          <input
-            className="mx-auto mb-5 flex h-[4.58vh] w-[35.55vw] items-center justify-center rounded-[40px] bg-[#FFFFFF] pl-16 placeholder:mr-[72px] placeholder:text-right"
-            value={email}
-            placeholder="ایمیل"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type={showPassword ? "text" : "password"}
-            className="mx-auto mb-5 flex h-[4.58vh] w-[35.55vw] items-center justify-center rounded-[40px] bg-[#FFFFFF] pl-16 placeholder:mr-[72px] placeholder:text-right"
-            value={password}
-            placeholder="رمز عبور (8 کاراکتر)"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-
-          <input
-            type={showRepeatPassword ? "text" : "password"}
-            className="mx-auto mb-5 flex h-[4.58vh] w-[35.55vw] items-center justify-center rounded-[40px] bg-[#FFFFFF] pl-16 placeholder:mr-[72px] placeholder:text-right"
-            value={repeatPassword}
-            placeholder="تکرار رمز عبور"
-            onChange={(e) => setRepeatPassword(e.target.value)}
-          />
-
-          <button className="mx-auto flex h-[38px] w-[143px] cursor-pointer items-center justify-center rounded-[46px] bg-[#2663CD] text-white">
->>>>>>> 228aa17fd3f1bf3008c45103cf9df45de73743f9:src/features/Auth/SignUp/signup.jsx
             ثبت نام
           </button>
         </form>
@@ -165,7 +122,6 @@ function Signup() {
           className="absolute right-[320px] top-[0px]"
         />
       </main>
-<<<<<<< HEAD:src/features/Auth/Sign up/signup.jsx
       <img
         src="/src/assets/images/bottom-left.png"
         alt="bottom-left"
@@ -175,74 +131,6 @@ function Signup() {
         src="/src/assets/images/bottom-right.png"
         alt="bottom-right"
         className=" absolute right-[0px] bottom-0 w-[33vw] ascept-auto"
-=======
-      {showPassword ? (
-        <img
-          src="/photos/eye-on.png"
-          alt="eye-on-password"
-          onClick={handleShowPassword}
-          className="absolute top-[36.75vh] left-[33vw] z-50"
-        />
-      ) : (
-        <img
-          src="/photos/eye-off.png"
-          alt="eye-off-password"
-          onClick={handleShowPassword}
-          className="absolute top-[36.75vh] left-[33vw] z-50"
-        />
-      )}
-      {showRepeatPassword ? (
-        <img
-          src="/photos/eye-on.png"
-          alt="eye-on-repeat-password"
-          onClick={handleShowRepeatPassword}
-          className="absolute top-[44vh] left-[33vw] z-50"
-        />
-      ) : (
-        <img
-          src="/photos/eye-off.png"
-          alt="eye-off-repeat-password"
-          onClick={handleShowRepeatPassword}
-          className="absolute top-[44vh] left-[33vw] z-50"
-        />
-      )}
-
-      <img
-        src="/photos/user.png"
-        alt="user"
-        className="absolute top-[29.5vh] right-[33vw]"
-      />
-      <img
-        src="/photos/lock.png"
-        alt="lock-password"
-        className="absolute top-[36.75vh] right-[33vw]"
-      />
-      <img
-        src="/photos/lock.png"
-        alt="lock-repeat-password"
-        className="absolute top-[44vh] right-[33vw]"
-      />
-
-      <img
-        src="/photos/mid-left.png"
-        alt="mid-left"
-        className="absolute bottom-[22.2vh] left-[23.5vw]"
-      />
-      <img
-        src="/photos/mid-right.png"
-        alt="mid-right"
-        className="absolute right-[18vw] bottom-[48vh]"
-      />
-      <img
-        src="/photos/bottom-left.png"
-        alt="bottom-left"
-        className="absolute bottom-[0vh] left-[0vw]"
-      />
-      <img
-        src="/photos/bottom-right.png"
-        alt="bottom-right"
-        className="absolute right-[1vw] bottom-[0vh]"
->>>>>>> 228aa17fd3f1bf3008c45103cf9df45de73743f9:src/features/Auth/SignUp/signup.jsx
       />
     </div>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,36 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router";
-<<<<<<< HEAD
-import Signup from "./features/Auth/Sign up/signup";
-import Login from "./features/Auth/Login/login";
-import Otp from "./features/Auth/Sign up/otpCheck";
-=======
 import Signup from "./features/Auth/SignUp/signup";
 import Login from "./features/Auth/Login/login";
+import Otp from "./features/Auth/SignUp/otpCheck.jsx";
 import Profile from "./features/Profiles/UserProfile/userprofile";
 import Another_User_Profile from "./features/Profiles/AnotherUserProfile/anotheruserprofile";
 import BookPage from "./features/Bookpage/Bookpage";
 import Forget_password from "./features/Auth/forget_password/Forget_password";
 import Vf from "./features/Auth/forget_password/Vf";
->>>>>>> 228aa17fd3f1bf3008c45103cf9df45de73743f9
 function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/auth/signup" element={<Signup />}></Route>
-<<<<<<< HEAD
         <Route path="/auth/otp" element={<Otp />}></Route>
         <Route path="/auth/login" element={<Login />}></Route>
-=======
-        <Route path="/auth/login" element={<Login />}></Route>
         <Route path="/profiles/userprofile" element={<Profile />}></Route>
-        <Route path="/book/:id" element={<BookPage/>}></Route>
-        <Route path='/Forget_password' element={ <Forget_password/>}/>
-        <Route path='/Vf' element={<Vf/>}/>
+        <Route path="/book/:id" element={<BookPage />}></Route>
+        <Route path="/Forget_password" element={<Forget_password />} />
+        <Route path="/Vf" element={<Vf />} />
         <Route
           path="/profiles/anotheruserprofile"
           element={<Another_User_Profile />}
         ></Route>
->>>>>>> 228aa17fd3f1bf3008c45103cf9df45de73743f9
       </Routes>
     </Router>
   );
