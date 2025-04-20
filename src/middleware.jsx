5e81e4a298e1a2a135620e38bf521cf68e08fa81
Alisalimi11 <www.asalimi1399@gmail.com>
2025-04-20 17:01:04 +0330

    favorite completed.

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router";
-import { useSelector } from "react-redux";
 export default function Middleware({ children }) {
   const { isAuthenticated } = useSelector((state) => state.auth);
   if (!isAuthenticated) {
