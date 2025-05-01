3b448a9142e3ab8b757a9ac34e4df29da61a1cc8
Alisalimi11 <www.asalimi1399@gmail.com>
2025-05-01 20:06:58 +0330

    navbar & footer refactor completed.

--- src/homepage/Homepage.jsx
+++ src/homepage/Homepage.jsx
@@ -18,37 +18,31 @@ export default function Homepage() {
 
   return (
     <div className="">
-      <Navbar className="" />
+      <Navbar />
       <SearchBar />
-
       <div className="flex flex-row justify-between ">
         <div className="sticky-content-l">
-          <Left></Left>
+          <Left />
         </div>
         <div className="">
           <h1 className="text-center my-10 text-2xl  "> کتاب های پرطرفدار </h1>
-          <Banner></Banner>
-
+          <Banner />
           <h1 className="text-center mb-8 text-4xl  "> تازه ترین ها </h1>
-          <Newest_books></Newest_books>
+          <Newest_books />
           <h1 className="text-center mb-8 text-4xl  "> پیشنهادی </h1>
-          <Suggestions></Suggestions>
+          <Suggestions />
           <h1 className="text-center my-10 text-2xl  ">
-            {" "}
-            محبوب ترین نویسنده ها{" "}
+            محبوب ترین نویسنده ها
           </h1>
           <Popular_authors></Popular_authors>
-          <h1 className="text-center my-10 text-2xl  ">
-            {" "}
-            فعالترین نویسنده ها{" "}
-          </h1>
-          <Most_active_authors></Most_active_authors>
+          <h1 className="text-center my-10 text-2xl  ">فعالترین نویسنده ها</h1>
+          <Most_active_authors />
         </div>
         <div className="sticky-content-r">
-          <Right></Right>
+          <Right />
         </div>
       </div>
-      <Footer> </Footer>
+      <Footer />
     </div>
   );
 }
