fdf71268048a03b461746398b8a81407d862b0ee
unknown <amirhasanpezhman@gmail.com>
2025-05-04 23:44:59 +0330

    mergin

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
- base: process.env.VITE_BASE_PATH || "/frontend",
+ 
   optimizeDeps: {
     exclude: ["lucide-react"],
 
