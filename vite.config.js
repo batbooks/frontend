889fc947579191f333a0a613321b5a04a351120a
unknown <amirhasanpezhman@gmail.com>
2025-05-21 22:50:07 +0330

    server back?

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "https://www.batbooks.ir/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
