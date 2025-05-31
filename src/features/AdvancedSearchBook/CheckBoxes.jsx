e2e4a7847661c4c7e27ca46b52364c707b4fab03
MaMaDDMD <M.H.damad@chmail.ir>
2025-05-31 19:23:43 +0330

    refactor advanced search book

---
+++ src/features/AdvancedSearchBook/CheckBoxes.jsx
@@ -0,0 +1,147 @@
+import { useSharedState } from "./SharedStateProvider";
+
+export function CheckBoxes({ addFilter, filters }) {
+  const { checkedNum, setCheckedNum } = useSharedState();
+
+  return (
+    <div className="flex flex-col gap-[17px]">
+      <h2 className="text-[20px] font-[300] ">وضعیت کتاب:</h2>
+      <div className="flex mr-[30px] gap-[25px]">
+        <div className="flex items-center gap-[5px]">
+          <button
+            onClick={() => {
+              if (checkedNum === -1) {
+                setCheckedNum(0);
+                addFilter([...(filters || []), "وضعیت: در حال تالیف"]);
+              } else if (checkedNum === 0) {
+                setCheckedNum(-1);
+                addFilter(
+                  filters?.filter((filter) => filter !== "وضعیت: در حال تالیف")
+                );
+              } else {
+                setCheckedNum(0);
+                addFilter((filters) =>
+                  filters?.filter(
+                    (filter) =>
+                      filter !== "وضعیت: به اتمام رسیده" &&
+                      filter !== "وضعیت: متوقف شده"
+                  )
+                );
+                addFilter((filters) => [
+                  ...(filters || []),
+                  "وضعیت: در حال تالیف",
+                ]);
+              }
+            }}
+            className="rounded-full cursor-pointer h-[30px] w-[30px] hover:bg-[#E5E5E5]/30 transition-colors duration-400 active:bg-[#E5E5E5]/50 active:duration-100"
+          >
+            {checkedNum === 0 ? (
+              <img
+                src="/src/assets/images/checked.png"
+                alt="checked"
+                className="h-[18px] w-[18px] mx-auto"
+              />
+            ) : (
+              <img
+                src="/src/assets/images/unchecked.png"
+                alt="unchecked"
+                className="h-[18px] w-[18px] mx-auto"
+              />
+            )}
+          </button>
+          <span className="text-[20px]  font-[400]">در حال تالیف</span>
+        </div>
+        <div className="flex items-center gap-[5px]">
+          <button
+            onClick={() => {
+              if (checkedNum === -1) {
+                setCheckedNum(1);
+                addFilter([...(filters || []), "وضعیت: به اتمام رسیده"]);
+              } else if (checkedNum === 1) {
+                setCheckedNum(-1);
+                addFilter(
+                  filters?.filter(
+                    (filter) => filter !== "وضعیت: به اتمام رسیده"
+                  )
+                );
+              } else {
+                setCheckedNum(1);
+                addFilter((filters) =>
+                  filters?.filter(
+                    (filter) =>
+                      filter !== "وضعیت: در حال تالیف" &&
+                      filter !== "وضعیت: متوقف شده"
+                  )
+                );
+                addFilter((filters) => [
+                  ...(filters || []),
+                  "وضعیت: به اتمام رسیده",
+                ]);
+              }
+            }}
+            className="rounded-full cursor-pointer h-[30px] w-[30px] hover:bg-[#E5E5E5]/30 transition-colors duration-400 active:bg-[#E5E5E5]/50 active:duration-100"
+          >
+            {checkedNum === 1 ? (
+              <img
+                src="/src/assets/images/checked.png"
+                alt="checked"
+                className="h-[18px] w-[18px] mx-auto"
+              />
+            ) : (
+              <img
+                src="/src/assets/images/unchecked.png"
+                alt="unchecked"
+                className="h-[18px] w-[18px] mx-auto"
+              />
+            )}
+          </button>
+          <span className="text-[20px]  font-[400]">به اتمام رسیده</span>
+        </div>
+        <div className="flex items-center gap-[5px]">
+          <button
+            onClick={() => {
+              if (checkedNum === -1) {
+                setCheckedNum(2);
+                addFilter([...(filters || []), "وضعیت: متوقف شده"]);
+              } else if (checkedNum === 2) {
+                setCheckedNum(-1);
+                addFilter(
+                  filters?.filter((filter) => filter !== "وضعیت: متوقف شده")
+                );
+              } else {
+                setCheckedNum(2);
+                addFilter((filters) =>
+                  filters?.filter(
+                    (filter) =>
+                      filter !== "وضعیت: در حال تالیف" &&
+                      filter !== "وضعیت: به اتمام رسیده"
+                  )
+                );
+                addFilter((filters) => [
+                  ...(filters || []),
+                  "وضعیت: متوقف شده",
+                ]);
+              }
+            }}
+            className="rounded-full cursor-pointer h-[30px] w-[30px] hover:bg-[#E5E5E5]/30 transition-colors duration-400 active:bg-[#E5E5E5]/50 active:duration-100"
+          >
+            {checkedNum === 2 ? (
+              <img
+                src="/src/assets/images/checked.png"
+                alt="checked"
+                className="h-[18px] w-[18px] mx-auto"
+              />
+            ) : (
+              <img
+                src="/src/assets/images/unchecked.png"
+                alt="unchecked"
+                className="h-[18px] w-[18px] mx-auto"
+              />
+            )}
+          </button>
+          <span className="text-[20px]  font-[400]">متوقف شده</span>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/features/AdvancedSearchBook/FromToInputs.jsx
@@ -0,0 +1,275 @@
+export function FromToInputs({
+  maxValue,
+  valueLength,
+  setFilters,
+  filterPattern,
+  fromValue,
+  setFromValue,
+  toValue,
+  setToValue,
+}) {
+  function deleteAllFilters() {
+    setFilters((filters) =>
+      filters?.filter((filter) =>
+        filterPattern === "chapter"
+          ? !filter.includes("تعداد فصل ها: از") &&
+            !filter.includes("تعداد فصل ها: تا") &&
+            !filter.includes("تعداد فصل ها: ")
+          : filterPattern === "fav"
+            ? !filter.includes("تعداد پسندیده ها: از") &&
+              !filter.includes("تعداد پسندیده ها: تا") &&
+              !filter.includes("تعداد پسندیده ها: ")
+            : filterPattern === "scorer"
+              ? !filter.includes("تعداد امتیازدهندگان: از") &&
+                !filter.includes("تعداد امتیازدهندگان: تا") &&
+                !filter.includes("تعداد امتیازدهندگان: ")
+              : null
+      )
+    );
+  }
+
+  function setFromFilters() {
+    deleteAllFilters();
+    if (toValue === "1") {
+      setFilters((filters) =>
+        filterPattern === "chapter"
+          ? [...(filters || []), "تعداد فصل ها: 1 فصل"]
+          : filterPattern === "fav"
+            ? [...(filters || []), "تعداد پسندیده ها: 1 نفر"]
+            : filterPattern === "scorer"
+              ? [...(filters || []), "تعداد امتیازدهندگان: 1 نفر"]
+              : null
+      );
+    } else {
+      setFilters((filters) =>
+        filterPattern === "chapter"
+          ? toValue !== maxValue
+            ? [...(filters || []), `تعداد فصل ها: تا ${toValue} فصل`]
+            : null
+          : filterPattern === "fav"
+            ? toValue !== maxValue
+              ? [...(filters || []), `تعداد پسندیده ها: تا ${toValue} نفر`]
+              : null
+            : filterPattern === "scorer"
+              ? toValue !== maxValue
+                ? [...(filters || []), `تعداد امتیازدهندگان: تا ${toValue} نفر`]
+                : null
+              : null
+      );
+    }
+  }
+
+  function setToFilters() {
+    deleteAllFilters();
+    if (fromValue === maxValue) {
+      setFilters((filters) =>
+        filterPattern === "chapter"
+          ? [...(filters || []), `تعداد فصل ها: ${maxValue} فصل`]
+          : filterPattern === "fav"
+            ? [...(filters || []), `تعداد پسندیده ها: ${maxValue} نفر`]
+            : filterPattern === "scorer"
+              ? [...(filters || []), `تعداد امتیازدهندگان: ${maxValue} نفر`]
+              : null
+      );
+    } else {
+      setFilters((filters) =>
+        filterPattern === "chapter"
+          ? fromValue !== "1"
+            ? [...(filters || []), `تعداد فصل ها: از ${fromValue} فصل`]
+            : null
+          : filterPattern === "fav"
+            ? fromValue !== "1"
+              ? [...(filters || []), `تعداد پسندیده ها: از ${fromValue} نفر`]
+              : null
+            : filterPattern === "scorer"
+              ? fromValue !== "1"
+                ? [
+                    ...(filters || []),
+                    `تعداد امتیازدهندگان: از ${fromValue} نفر`,
+                  ]
+                : null
+              : null
+      );
+    }
+  }
+
+  function setEqualFilter(fromOrTo) {
+    deleteAllFilters();
+    setFilters((filters) =>
+      filterPattern === "chapter"
+        ? [
+            ...(filters || []),
+            `تعداد فصل ها: ${fromOrTo === "from" ? fromValue : toValue} فصل`,
+          ]
+        : filterPattern === "fav"
+          ? [
+              ...(filters || []),
+              `تعداد پسندیده ها: ${fromOrTo === "from" ? fromValue : toValue} نفر`,
+            ]
+          : filterPattern === "scorer"
+            ? [
+                ...(filters || []),
+                `تعداد امتیازدهندگان: ${fromOrTo === "from" ? fromValue : toValue} نفر`,
+              ]
+            : null
+    );
+  }
+
+  function setNotEqualFilter(numWithoutLeadingZeros, fromOrTo) {
+    deleteAllFilters();
+    setFilters((filters) =>
+      filterPattern === "chapter"
+        ? fromOrTo === "from"
+          ? [
+              ...(filters || []),
+              fromValue !== "1"
+                ? `تعداد فصل ها: از ${fromValue} فصل`
+                : undefined,
+              numWithoutLeadingZeros !== Number(maxValue)
+                ? `تعداد فصل ها: تا ${numWithoutLeadingZeros} فصل`
+                : undefined,
+            ].filter((item) => item !== undefined)
+          : [
+              ...(filters || []),
+              numWithoutLeadingZeros !== 1
+                ? `تعداد فصل ها: از ${numWithoutLeadingZeros} فصل`
+                : undefined,
+              toValue !== maxValue
+                ? `تعداد فصل ها: تا ${toValue} فصل`
+                : undefined,
+            ].filter((item) => item !== undefined)
+        : filterPattern === "fav"
+          ? fromOrTo === "from"
+            ? [
+                ...(filters || []),
+                fromValue !== "1"
+                  ? `تعداد پسندیده ها: از ${fromValue} نفر`
+                  : undefined,
+                numWithoutLeadingZeros !== Number(maxValue)
+                  ? `تعداد پسندیده ها: تا ${numWithoutLeadingZeros} نفر`
+                  : undefined,
+              ].filter((item) => item !== undefined)
+            : [
+                ...(filters || []),
+                numWithoutLeadingZeros !== 1
+                  ? `تعداد پسندیده ها: از ${numWithoutLeadingZeros} نفر`
+                  : undefined,
+                toValue !== maxValue
+                  ? `تعداد پسندیده ها: تا ${toValue} نفر`
+                  : undefined,
+              ].filter((item) => item !== undefined)
+          : filterPattern === "scorer"
+            ? fromOrTo === "from"
+              ? [
+                  ...(filters || []),
+                  fromValue !== "1"
+                    ? `تعداد امتیازدهندگان: از ${fromValue} نفر`
+                    : undefined,
+                  numWithoutLeadingZeros !== Number(maxValue)
+                    ? `تعداد امتیازدهندگان: تا ${numWithoutLeadingZeros} نفر`
+                    : undefined,
+                ].filter((item) => item !== undefined)
+              : [
+                  ...(filters || []),
+                  numWithoutLeadingZeros !== 1
+                    ? `تعداد امتیازدهندگان: از ${numWithoutLeadingZeros} نفر`
+                    : undefined,
+                  toValue !== maxValue
+                    ? `تعداد امتیازدهندگان: تا ${toValue} نفر`
+                    : undefined,
+                ].filter((item) => item !== undefined)
+            : null
+    );
+  }
+
+  function handleFromValue() {
+    if (fromValue.trim() === "" || /^0+$/.test(fromValue) || isNaN(fromValue)) {
+      setFromValue("1");
+      setFromFilters();
+      return;
+    }
+    const num = Number(fromValue);
+    if (num < 0 || !Number.isInteger(num) || num === 0) {
+      setFromValue("1");
+      setFromFilters();
+      return;
+    }
+    const numWithoutLeadingZeros = parseInt(fromValue, 10);
+    if (numWithoutLeadingZeros >= Number(toValue)) {
+      setFromValue(toValue);
+      setEqualFilter("to");
+      return;
+    }
+    setFromValue(`${numWithoutLeadingZeros}`);
+    setNotEqualFilter(numWithoutLeadingZeros, "to");
+  }
+
+  function handleToValue() {
+    if (toValue.trim() === "" || /^0+$/.test(toValue) || isNaN(toValue)) {
+      setToValue(maxValue);
+      setToFilters();
+      return;
+    }
+    const num = Number(toValue);
+    if (num < 0 || !Number.isInteger(num) || num === 0) {
+      setToValue(maxValue);
+      setToFilters();
+      return;
+    }
+    const numWithoutLeadingZeros = parseInt(toValue, 10);
+    if (numWithoutLeadingZeros <= Number(fromValue)) {
+      setToValue(fromValue);
+      setEqualFilter("from");
+      return;
+    }
+    setToValue(`${numWithoutLeadingZeros}`);
+    setNotEqualFilter(numWithoutLeadingZeros, "from");
+  }
+
+  return (
+    <div className="flex justify-between mr-[30px] gap-[76px]">
+      <div className="flex items-center gap-[9px]">
+        <span className="text-[20px] font-[300] ">از:</span>
+        <input
+          dir="ltr"
+          value={fromValue}
+          onChange={(e) =>
+            e.target.value.length <= valueLength &&
+            !isNaN(Number(e.target.value))
+              ? setFromValue(e.target.value)
+              : null
+          }
+          onBlur={() => handleFromValue()}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleFromValue();
+              e.target.blur();
+            }
+          }}
+          className="h-[40px] w-[80px] bg-white rounded-[5px] text-center outline-[2px] outline-[#000000]/21 focus:outline-[3px] focus:outline-[#2663CD]"
+        />
+      </div>
+      <div className="flex items-center gap-[9px]">
+        <span className="text-[20px] font-[300] ">تا:</span>
+        <input
+          dir="ltr"
+          value={toValue}
+          onChange={(e) =>
+            e.target.value.length <= valueLength &&
+            !isNaN(Number(e.target.value))
+              ? setToValue(e.target.value)
+              : null
+          }
+          onBlur={() => handleToValue()}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleToValue();
+              e.target.blur();
+            }
+          }}
+          className="h-[40px] w-[80px] bg-white rounded-[5px] text-center outline-[2px] outline-[#000000]/21 focus:outline-[3px] focus:outline-[#2663CD]"
+        />
+      </div>
+    </div>
+  );
+}
